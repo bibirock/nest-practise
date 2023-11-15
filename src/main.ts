c0baf1c92a8da1be366e4444ea8f2378a3be0fe5
Joe.Chen <joechen@tracle-tw.com>
2023-11-15 14:29:52 +0800

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,15 @@
+/*
+ * @Author: Joe.Chen
+ * @Date: 2023-11-15 10:01:37
+ * @LastEditors: Joe.Chen joechen@tracle-tw.com
+ * @LastEditTime: 2023-11-15 10:55:21
+ * @Description:
+ */
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+
+// main.ts 為進入點檔案，這裡會呼叫工具函示將 app 給建立起來，並且監聽 port 3000 的變化
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
