af0fdf0d7babed91087d45d1db27ba2036f6a727
Joe.Chen <joechen@tracle-tw.com>
2023-11-15 16:03:31 +0800

    [feature]: 新增 Dto 資料傳輸物件

---
+++ src/todo/dto/create-todo.dto.ts
@@ -0,0 +1,12 @@
+/*
+ * @Author: Joe.Chen
+ * @Date: 2023-11-15 14:59:27
+ * @LastEditors: Joe.Chen joechen@tracle-tw.com
+ * @LastEditTime: 2023-11-15 15:02:19
+ * @Description:
+ */
+
+export class CreateTodoDto {
+  readonly title: string;
+  readonly description?: string;
+}
--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -2,17 +2,18 @@
  * @Author: Joe.Chen
  * @Date: 2023-11-15 11:42:26
  * @LastEditors: Joe.Chen joechen@tracle-tw.com
- * @LastEditTime: 2023-11-15 14:47:54
+ * @LastEditTime: 2023-11-15 15:03:21
  * @Description:
  */
 
 import { Body, Controller, Post } from '@nestjs/common';
+import { CreateTodoDto } from './dto/create-todo.dto';
 
 @Controller('todo')
 export class TodoController {
   @Post()
-  create(@Body() data: { title: string; description?: string }) {
+  create(@Body() dto: CreateTodoDto) {
     const id = 1;
-    return { id, ...data };
+    return { id, ...dto };
   }
 }
