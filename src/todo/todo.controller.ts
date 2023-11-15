4b23ed9860b5f98794ca5d6b2ea1d21bd2f2c538
Joe.Chen <joechen@tracle-tw.com>
2023-11-15 14:48:27 +0800

    [feature]: 新增 Post 取得 Body 內容

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -2,58 +2,17 @@
  * @Author: Joe.Chen
  * @Date: 2023-11-15 11:42:26
  * @LastEditors: Joe.Chen joechen@tracle-tw.com
- * @LastEditTime: 2023-11-15 14:19:02
+ * @LastEditTime: 2023-11-15 14:47:54
  * @Description:
  */
-import { Controller, Patch, HttpCode, HttpStatus } from '@nestjs/common';
+
+import { Body, Controller, Post } from '@nestjs/common';
 
 @Controller('todo')
 export class TodoController {
-  // @Get('/examples') // 裡面建立子路由
-  // getExample() {
-  //   return [
-  //     {
-  //       id: 1,
-  //       title: 'Example 1',
-  //       description: '',
-  //     },
-  //   ];
-  // }
-
-  // 路由參數設定
-  // @Get('/examples/test/')
-  // getTest(@Param() params: { test1: string }) {
-  //   const { test1 } = params;
-  //   return [
-  //     {
-  //       title: `Title ${test1}`,
-  //       test: '123456',
-  //     },
-  //   ];
-  // }
-
-  // 路由查詢設定
-  // getTest(@Query('limit') limit = 30, @Query('skip') skip = 0) {
-  //   const list = [
-  //     {
-  //       id: `${limit}`,
-  //       title: 'Title 1',
-  //       description: '',
-  //     },
-  //     {
-  //       id: `${skip}`,
-  //       title: 'Title 2',
-  //       description: '',
-  //     },
-  //   ];
-
-  //   return list.slice(skip, limit);
-  // }
-
-  // 狀態碼設置
-  @Patch('/')
-  @HttpCode(HttpStatus.BAD_GATEWAY)
-  get() {
-    return [];
+  @Post()
+  create(@Body() data: { title: string; description?: string }) {
+    const id = 1;
+    return { id, ...data };
   }
 }
